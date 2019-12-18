97063cabece40a211ac9e6652f7ed44a49c31337
Izergin <i.izergin@tinkoff.ru>
2019-12-19 00:57:49 +0300

    Person class + one test

--- spring-01-exercise/src/main/java/ru/otus/spring/service/Examination.java
+++ spring-01-exercise/src/main/java/ru/otus/spring/service/Examination.java
@@ -4,6 +4,7 @@ import au.com.bytecode.opencsv.bean.CsvToBean;
 import au.com.bytecode.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
 import lombok.AllArgsConstructor;
 import org.springframework.core.io.Resource;
+import ru.otus.spring.junit.domain.Person;
 
 import java.io.*;
 import java.util.*;
@@ -39,21 +40,15 @@ public class Examination {
             //познакомимся с пользователем
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("What is your name?");
-            String userName = br.readLine();
+            Person person = new Person(br.readLine());
+
             int questionsCnt = questionsList.size();
             int rightAnswers = 0;
             for (Question e : questionsList) {
                 //Начинаем задавать вопросы
-                System.out.println(e.getQuestion());
-                String answer = br.readLine();
-                if (answer.equals(e.getTrueAnswer())) {
-                    System.out.println("You are right");
-                    rightAnswers++;
-                } else {
-                    System.out.println("You are wrong");
-                }
+                rightAnswers += e.ask();
             }
-            System.out.println("You (" + userName + ") got " + rightAnswers + " out of " + questionsCnt +" questions!");
+            System.out.println("You (" + person.getName() + ") got " + rightAnswers + " out of " + questionsCnt +" questions!");
         } catch (Exception e) {
             System.out.println(e.fillInStackTrace());
         }
---
+++ spring-01-exercise/src/test/java/ru/otus/spring/junit/domain/PersonTest.java
@@ -0,0 +1,19 @@
+package ru.otus.spring.junit.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("Класс Person")
+class PersonTest {
+    @DisplayName("корректно создаётся конструктором")
+    @Test
+    void testConstructorWithAsserts() {
+        Person person = new Person("Ivan");
+        assertThat(person.getName())
+                .startsWith("Iv")
+                .endsWith("an")
+                .isEqualTo("Ivan")
+                .hasSize("Ivan".length());
+    }
+}
