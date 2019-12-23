0db5c9def08c2949b3b6a3d72adbfadacf14d6d5
Izergin <i.izergin@tinkoff.ru>
2019-12-24 00:50:46 +0300

    ex02 localization added

--- spring-01-exercise/src/main/java/ru/otus/spring/Main.java
+++ spring-01-exercise/src/main/java/ru/otus/spring/Main.java
@@ -3,6 +3,7 @@ package ru.otus.spring;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import ru.otus.spring.component.LocaleHolder;
 import ru.otus.spring.controller.ExamController;
 import ru.otus.spring.domain.Person;
 import ru.otus.spring.domain.Question;
@@ -10,41 +11,75 @@ import ru.otus.spring.domain.Question;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Locale;
 
 @ComponentScan
 @Configuration
 public class Main {
+    private static LocaleHolder lh;
+    private static Person person;
+
     public static void main(String[] args) {
+        //познакомимся с пользователем
+        person = new Person("Igor");
+
+        askStudentForLocale();
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-        ExamController ec = context.getBean(ExamController.class);
-//        List<Question> qList = ec.getQuestionsByExamName("math");
-        List<Question> qList = ec.getQuestionsByExamName("biology");
-        if (qList == null) {return;}
+        lh = context.getBean(LocaleHolder.class);
 
-        //познакомимся с пользователем
-        Person person = new Person("Igor");
+        System.out.println(lh.getMessage("hello.user", new String[]{person.getName()}, Locale.getDefault()));
+
+        ExamController ec = context.getBean(ExamController.class);
+        List<Question> qList = ec.getQuestionsByExamName("math");
+        //List<Question> qList = ec.getQuestionsByExamName("biology");
+        if (qList == null) {
+            return;
+        }
 
         //задать вопросы
         int rightAnswers = 0;
         for (Question e : qList) {
             rightAnswers += ask(e);
         }
-        System.out.println("You (" + person.getName() + ") got " + rightAnswers + " out of " + qList.size() +" questions!");
+        System.out.println(lh.getMessage("exams.result", new String[]{person.getName(), rightAnswers + "", qList.size() + ""}, Locale.getDefault()));
     }
 
-    //задача 1 вопрос пользователю возвращает 1 если ответ верный, 0 - в других случаях
-    private static int ask (Question question) {
+    //при запуске приложения спрашиваем студента на каком языке он говорит
+    private static void askStudentForLocale() {
+        boolean defined = false;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (!defined) {
+            System.out.println("What language do you speak? Russian(ru) or English(en)?");
+            try {
+                String lang = br.readLine();
+                if (lang.equals("ru")) {
+                    Locale.setDefault(new Locale("ru", "RU"));
+                    defined = true;
+                } else if (lang.equals("en")) {
+                    Locale.setDefault(Locale.ENGLISH);
+                    defined = true;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    //задаем 1 вопрос пользователю, возвращает 1 если ответ верный, 0 - в других случаях
+    private static int ask(Question question) {
         int res;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(question.getQuestion());
         try {
             String answer = br.readLine();
             if (answer.equals(question.getTrueAnswer())) {
-                System.out.println("You are right");
+                //System.out.println("You are right");
+                System.out.println(lh.getMessage("exams.answer.right", null, Locale.getDefault()));
                 res = 1;
             } else {
-                System.out.println("You are wrong");
+                //System.out.println("You are wrong");
+                System.out.println(lh.getMessage("exams.answer.wrong", null, Locale.getDefault()));
                 res = 0;
             }
         } catch (Exception e) {
--- spring-01-exercise/src/main/java/ru/otus/spring/service/ExamService.java
+++ spring-01-exercise/src/main/java/ru/otus/spring/service/ExamService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
+import ru.otus.spring.component.LocaleHolder;
 import ru.otus.spring.dao.ExamQuestionsFileDao;
 import ru.otus.spring.domain.ExamProperties;
 import ru.otus.spring.domain.Question;
@@ -15,11 +16,13 @@ import java.util.*;
 public class ExamService {
     private final List<String> examsList;
     private final ExamQuestionsFileDao examQuestionsFileDao;
+    private final LocaleHolder localeHolder;
 
     @Autowired
-    public ExamService(ExamQuestionsFileDao examQuestionsFileDao, @Value("${exams.list}") String[] examsList) {
+    public ExamService(ExamQuestionsFileDao examQuestionsFileDao, @Value("${exams.list}") String[] examsList, LocaleHolder localeHolder) {
         this.examQuestionsFileDao = examQuestionsFileDao;
         this.examsList = Arrays.asList(examsList);
+        this.localeHolder = localeHolder;
     }
 
     public List<Question> getQuestionsByExamName(String examName) {
@@ -31,9 +34,10 @@ public class ExamService {
             //реализовать отдельный DAO который по экзамену выведет источник вопросов + настройки источника.
             // Пока предположим что возвращается источник CSV с указанным путем до файла. Бин DAO для доставания из базы тоже пока не реализую
             // создам конфигурационный файл искуственно
+            Locale l = Locale.getDefault();
+            String questionPath = localeHolder.getMessage("exams.csv.path."+examName, new String[]{"Ivan"},  Locale.getDefault());
             Map<String, ExamProperties> examPropertiesMap = new HashMap<>();
-            examPropertiesMap.put("math", new ExamProperties("csv", "/questionsMath.csv"));
-            examPropertiesMap.put("biology", new ExamProperties("csv", "/questionsBiology.csv"));
+            examPropertiesMap.put(examName, new ExamProperties("csv", questionPath));
 
             if (examPropertiesMap.get(examName).getExamQuestionsSource().equals("csv")) {
                 return examQuestionsFileDao.getQuestionsByFilePath(examPropertiesMap.get(examName).getExamQuestionsPath());
